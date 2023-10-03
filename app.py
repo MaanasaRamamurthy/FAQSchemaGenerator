fedb1e11cb10308bbea98a15a89aaf931aae1a46
MaanasaRamamurthy <maanasaramamurthy@gmail.com>
2023-10-03 19:42:25 +0530

    new commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 api_key = os.getenv('OPENAI_API_KEY')
 
 # Define your template question
-template_question = "Generate FAQ Schema in HTML for the following questions and answers: '{}'"
+template_question = "Generate FAQ Schema in microdata format for the following questions and answers: '{}'"
 
 def split_faq_input(input_text):
     faq_pairs = input_text.split('[QUESTION]')
@@ -22,7 +22,7 @@ def main():
     user_question = st.text_area("Enter questions and answers (format: [QUESTION]Your question[ANSWER]Your answer)", "")
     faq_pairs = split_faq_input(user_question)
     # Define a system message to set the context
-    system_message = "FAQ Schema Generator in HTML code in microdata format"
+    system_message = "FAQ Schema Generator in microdata format"
 
     if st.button("Generate FAQ Schema"):
         if user_question:
